4bc84c59fd087e21bbec06392e8f0597d5034089
Martine Aoun <77461046+martinea8@users.noreply.github.com>
2021-08-21 19:14:55 +0300

    zyro files

---
+++ zyro/1.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Facebook Open Graph -->
+	<meta property="og:title" content="Martine Aoun | Personal Website" />
+	<meta property="og:description" content="Martine Aoun | Personal Website" />
+	<meta property="og:image" content="http://martineaoun.com/gallery_gen//48186f25815892120f0207c94c27f806_400x455.1724137931.jpg" />
+	<meta property="og:type" content="article" />
+	<meta property="og:url" content="{{curr_url}}" />
+	<!-- Facebook Open Graph end -->    
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="title" content="Martine Aoun | Personal Website">
+	<title>Martine Aoun | Personal Website</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<meta name="description" content="Martine Aoun | Personal Website" />
+    <meta name="keywords" content="Martine Aoun,MartineAoun,Martine,Aoun,Aounmartine,Aounmartine86,martine.aoun,Martine Aoun Website,Personal Website,software engineer,software,ecotourism,science,nature,hiking,contact,website,beirut,lebanon" />
+    <meta name="thumbnail" content="http://martineaoun.com/gallery_gen//48186f25815892120f0207c94c27f806_400x455.1724137931.jpg" />
+    
+	<base href="{{base_url}}" />
+	
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js?v=1.0.8" type="text/javascript"></script>
+
+	<link href="css/site.css?v=1.1.52" rel="stylesheet" type="text/css" />
+	<link href="css/common.css?ts=1502035140" rel="stylesheet" type="text/css" />
+	<link href="css/1.css?ts=1502035140" rel="stylesheet" type="text/css" />
+	<link rel="shortcut icon" href="/gallery/favicon.jpg" type="image/jpeg" />
+	<script type="text/javascript">var currLang = '';</script>
+	<script type="text/javascript">var mainHeaderHeight = 68;</script>		
+	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+
+<body>{{ga_code}}<div class="root"><div id="wb_bgs_cont"><div id="wb_page_1_bg"></div><div id="wb_page_5_bg"></div><div id="wb_page_4_bg"></div></div><div id="wb_header_bg"></div><div id="wb_sbg_placeholder"></div><div class="vbox wb_container wb_header_fixed" id="wb_header">
+	
+<div id="wb_element_instance0" class="wb_element wb-menu wb-menu-mobile"><a class="btn btn-default btn-collapser"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a><ul class="hmenu menu-landing"><li class="active"><a href="#home" target="_self" title="Home">Home</a></li><li><a href="#my-story" target="_self" title="My Story">My Story</a></li><li><a href="#contact-me" target="_self" title="Contact Me">Contact Me</a></li></ul><div class="clearfix"></div></div><div id="wb_element_instance2" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal"><strong><span style="color:#32444e;">Martine Aoun</span></strong></p>
+</div></div>
+<div class="vbox wb_container" id="wb_main">
+	
+<div class="wb_page" id="page_1"><a name="home" class="wb_page_anchor" id="page_1_anchor"></a><div id="wb_element_instance3" class="wb_element" style=" line-height: normal;"><h2 class="wb-stl-heading2" style="text-align: center;"><span style="color:#32444e;">ABOUT ME</span></h2>
+</div><div id="wb_element_instance4" class="wb_element wb-elm-orient-horizontal"><div class="wb-elm-line"></div></div><div id="wb_element_instance5" class="wb_element wb_element_shape"><div class="wb_shp"></div></div><div id="wb_element_instance6" class="wb_element wb_element_picture"><img alt="Martine Aoun" src="gallery_gen//48186f25815892120f0207c94c27f806_400x455.1724137931.jpg"></div><!--<div id="wb_element_instance7" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal"><font color="#333333">I'm</font></p></div>--><div id="wb_element_instance8" class="wb_element wb_element_shape"><div class="wb_shp"></div></div><div id="wb_element_instance9" class="wb_element" style=" line-height: normal;"><p class="wb_stl_custom"><span style="color:#333333;">Sr. Software Engineer</span><br><span style="color:#333333;">Ecotourism Advisor</span></p>
+</div><div id="wb_element_instance10" class="wb_element wb-elm-orient-horizontal"><div class="wb-elm-line"></div></div><!--<div id="wb_element_instance11" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal"><span style="color:#333333;"></span></p>
+</div>--> <div id="wb_element_instance12" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal"><span style="color:#333333;"><span style="background-color: transparent;">Beirut, Lebanon</span></span></p>
+</div><div id="wb_element_instance13" class="wb_element" style=" line-height: normal;"></div><div id="wb_element_instance14" class="wb_element wb_element_picture"><a href="https://www.facebook.com/MartineAoun" title="Facebook" target="1"><img alt="Find me on Facebook" src="gallery_gen//83a32aa35b1a44f8c13a7c654bb6a776_32x32.png" class="sm_icon"></a></div><div id="wb_element_instance15" class="wb_element wb_element_picture"><a href="https://www.linkedin.com/in/martineaoun" title="LinkedIn" target="1"><img alt="Find me on LinkedIn" src="gallery_gen//db36b79c3cd184fd05ad236dbbe888d4_32x32.png" class="sm_icon"></a></div><div id="wb_element_instance16" class="wb_element wb_element_picture"><a href="https://www.instagram.com/MartineAoun/" title="Instagram" target="1"><img alt="Find me on Instagram" src="gallery_gen//1e5a88e6f832c0fa3d6137067f498d57_32x32.png" class="sm_icon"></a></div><div id="wb_element_instance17" class="wb_element wb_element_picture"><a href="https://twitter.com/MartineAoun" title="Twitter" target="1"><img alt="Find me on Twitter" src="gallery_gen//4ff5d087dc795c5c8066dd0a7e2423b9_32x32.png" class="sm_icon"></a></div><div id="wb_element_instance18" class="wb_element wb_element_picture"><a href="https://martineaoun.wordpress.com/" title="WordPress" target="1"><img alt="Find me on WordPress" src="gallery_gen//wordpress-26.png" class="sm_icon"></a></div><!--<div id="wb_element_instance19" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal" style="text-align: center;"><span style="color:#ffffff; vertical-align: text-top;"><strong>Hey!</strong></span></p>
+</div><div id="wb_element_instance20" class="wb_element wb_element_picture"><img src="gallery_gen//9254f5dea08120b70a0a81707353302f_285x140.png"></div>--><div id="wb_element_instance21" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal"><strong><span style="color:#333333;">Martine Aoun</span></strong></p>
+</div></div><div class="wb_page" id="page_5"><a name="my-story" class="wb_page_anchor" id="page_5_anchor"></a><div id="wb_element_instance22" class="wb_element" style=" line-height: normal;"><h2 class="wb-stl-heading2" style="text-align: center;"><font color="#ffffff">MY STORY</font></h2>
+</div><div id="wb_element_instance23" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal" style="text-align: center;"><font color="#ffffff">You will find the latest information about me on this page. If you want to contact me, please fill the contact form below.<br/>Thank you!</font></p>
+</div><div id="wb_element_instance24" class="wb_element" style=" line-height: normal;"></div></div><div class="wb_page" id="page_4"><a name="contact-me" class="wb_page_anchor" id="page_4_anchor"></a><div id="wb_element_instance25" class="wb_element wb-map"><script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&amp;key=AIzaSyAAKyLWPB8HDVQPGXNErjNe2yoeM8jTPm8&amp;sensor=false&amp;libraries=places&amp;region=US&amp;language=en_US"></script><script type="text/javascript">
+				function initialize() {
+					if (window.google) {
+						var div = document.getElementById("wb_element_instance25");
+						var ll = new google.maps.LatLng(33.8937913,35.50177669999994);
+						var map = new google.maps.Map(div, {
+							zoom: 16,
+							center: ll,
+							mapTypeId: "roadmap"
+						});
+						
+						var marker = new google.maps.Marker({
+							position: ll,
+							clickable: false,
+							map: map
+						});
+					}
+				}
+				google.maps.event.addDomListener(window, "load", initialize);
+			</script></div><div id="wb_element_instance26" class="wb_element" style=" line-height: normal;"><a name="Contacts" class="wb_anchor" style="top: -68px"></a><h2 class="wb-stl-heading2" style="text-align: center;"><span style="color:#ffffff;">Contact ME</span></h2>
+</div><div id="wb_element_instance27" class="wb_element wb_element_shape"><div class="wb_shp"></div></div><div id="wb_element_instance28" class="wb_element"><form class="wb_form wb_mob_form" method="post"><input type="hidden" name="wb_form_id" value="2236b1ae"><textarea name="message" rows="3" cols="20" class="hpc"></textarea><table><tr><th class="wb-stl-normal">Name&nbsp;&nbsp;</th><td><input type="hidden" name="wb_input_0" value="Name"><input class="form-control form-field" type="text" value="" name="wb_input_0" required="required"></td></tr><tr><th class="wb-stl-normal">Email&nbsp;&nbsp;</th><td><input type="hidden" name="wb_input_1" value="E-mail"><input class="form-control form-field" type="email" value="" name="wb_input_1" required="required"></td></tr><tr class="area-row"><th class="wb-stl-normal">Message&nbsp;&nbsp;</th><td><input type="hidden" name="wb_input_2" value="Message"><textarea class="form-control form-field form-area-field" rows="3" cols="20" name="wb_input_2" required="required"></textarea></td></tr><tr><th>&nbsp;</th><td><div id="wb_element_instance28_captcha" class="g-recaptcha" data-sitekey="6LeJlRMUAAAAALBKn4aZDXpju97_28gw_hNU_lF9"></div></td></tr><tr class="form-footer"><td colspan="2"><button type="submit" class="btn btn-default">Send</button></td></tr></table></form><script type="text/javascript">
+			<?php global $wb_form_id; if (isset($wb_form_id) & $wb_form_id == "2236b1ae") { ?>
+				var formValues = <?php echo json_encode($_POST); ?>;
+				var formErrors = <?php global $formErrors; echo json_encode($formErrors); ?>;
+				wb_form_validateForm("2236b1ae", formValues, formErrors);
+				<?php global $wb_form_send_state; if (isset($wb_form_send_state) && $wb_form_send_state) { ?>
+					setTimeout(function() {
+						alert("<?php echo addcslashes($wb_form_send_state, "\\'\"&\n\r\0\t<>"); ?>");
+					}, 1);
+					<?php $wb_form_send_state = null; ?>
+				<?php } ?>
+			<?php } ?>
+			</script></div><div id="wb_element_instance29" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal"><strong><span style="color:#333333;">Contact me here</span></strong></p></div><div id="wb_element_instance30" class="wb_element wb_element_shape"><div class="wb_shp"></div></div><div id="wb_element_instance31" class="wb_element wb-elm-orient-horizontal"><div class="wb-elm-line"></div></div><div id="wb_element_instance32" class="wb_element wb-elm-orient-horizontal"><div class="wb-elm-line"></div></div><div id="wb_element_instance33" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal"><span style="color:#333333;">Beirut</span></p>
+
+<p class="wb-stl-normal">Lebanon</p>
+</div></div><div id="wb_element_instance34" class="wb_element" style="width: 100%;">
+			<?php
+				global $show_comments;
+				if (isset($show_comments) && $show_comments) {
+					renderComments(1);
+			?>
+			<script type="text/javascript">
+				$(function() {
+					var block = $("#wb_element_instance34");
+					var comments = block.children(".wb_comments").eq(0);
+					var contentBlock = $("#wb_main");
+					contentBlock.height(contentBlock.height() + comments.height());
+				});
+			</script>
+			<?php
+				} else {
+			?>
+			<script type="text/javascript">
+				$(function() {
+					$("#wb_element_instance34").hide();
+				});
+			</script>
+			<?php
+				}
+			?>
+			</div></div>
+<div class="vbox wb_container" id="wb_footer" style="height: 119px;">
+	
+<div id="wb_element_instance1" class="wb_element" style=" line-height: normal;"><p class="wb-stl-footer">© 2017 <a href="http://martineaoun.com">martineaoun.com</a></p>
+</div><div id="wb_element_instance35" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#wb_footer");
+					footer.css({height: ""});
+				}
+			});
+			</script></div></div><div class="wb_sbg"></div></div><script type="text/javascript">
+(function() { 
+window.RecaptchaCallback = function() { 
+grecaptcha.render("wb_element_instance28_captcha", {"sitekey": "6LeJlRMUAAAAALBKn4aZDXpju97_28gw_hNU_lF9"});
+}; 
+})(); 
+</script><script type="text/javascript" src="https://www.google.com/recaptcha/api.js?onload=RecaptchaCallback&amp;render=explicit"></script>{{hr_out}}</body>
+</html>
---
+++ zyro/blog.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Blog</title>
+	<base href="{{base_url}}" />
+			<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+	<meta name="keywords" content="Blog" />
+	<!-- Facebook Open Graph -->
+	<meta name="og:title" content="Blog" />
+	<meta name="og:description" content="" />
+	<meta name="og:image" content="" />
+	<meta name="og:type" content="article" />
+	<meta name="og:url" content="{{curr_url}}" />
+	<!-- Facebook Open Graph end -->
+		
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js?v=1.0.8" type="text/javascript"></script>
+
+	<link href="css/site.css?v=1.1.52" rel="stylesheet" type="text/css" />
+	<link href="css/common.css?ts=1502035140" rel="stylesheet" type="text/css" />
+	<link href="css/blog.css?ts=1502035140" rel="stylesheet" type="text/css" />
+	<meta name="title" content="Martine Aoun Website - Personal Website"><link rel="shortcut icon" href="/gallery/favicon.jpg" type="image/jpeg" />
+	<script type="text/javascript">var currLang = '';</script>		
+	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+
+<body>{{ga_code}}<div class="root"><div class="vbox wb_container" id="wb_header">
+	
+<div id="wb_element_instance40" class="wb_element wb-menu wb-menu-mobile"><a class="btn btn-default btn-collapser"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a><ul class="hmenu menu-landing"><li><a href="#home" target="_self" title="Home">Home</a></li><li><a href="#my-story" target="_self" title="My Story">My Story</a></li><li><a href="#contact-me" target="_self" title="Contact Me">Contact Me</a></li></ul><div class="clearfix"></div></div></div>
+<div class="vbox wb_container" id="wb_main">
+	
+<div id="wb_element_instance42" class="wb_element" style="width: 100%;">
+			<?php
+				global $show_comments;
+				if (isset($show_comments) && $show_comments) {
+					renderComments(blog);
+			?>
+			<script type="text/javascript">
+				$(function() {
+					var block = $("#wb_element_instance42");
+					var comments = block.children(".wb_comments").eq(0);
+					var contentBlock = $("#wb_main");
+					contentBlock.height(contentBlock.height() + comments.height());
+				});
+			</script>
+			<?php
+				} else {
+			?>
+			<script type="text/javascript">
+				$(function() {
+					$("#wb_element_instance42").hide();
+				});
+			</script>
+			<?php
+				}
+			?>
+			</div></div>
+<div class="vbox wb_container" id="wb_footer" style="height: 119px;">
+	
+<div id="wb_element_instance41" class="wb_element" style=" line-height: normal;"><p class="wb-stl-footer">© 2017 <a href="http://martineaoun.com">martineaoun.com</a></p>
+</div><div id="wb_element_instance43" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#wb_footer");
+					footer.css({height: ""});
+				}
+			});
+			</script></div></div><div class="wb_sbg"></div></div>{{hr_out}}</body>
+</html>
